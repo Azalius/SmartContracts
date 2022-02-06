97238683769f4115e9f3204f1640c8e13fb3bb33
Azalius <mateo1998@live.fr>
2022-02-06 21:23:20 +0100

    DemocraticWallet written but untested

--- contracts/DemocraticWallet.sol
+++ contracts/DemocraticWallet.sol
@@ -7,12 +7,14 @@ pragma solidity >=0.6.0;
 // Dynamic modifications of the voters add some complexity (& gas cost)
 contract DemocraticWallet{
 
-    //all the adresses allowed to vote
+    //all the adresses allowed to vote, bot as litst & mapping for gas efficiency
     mapping(address=>bool) public voters;
+    address[] votersList;
+
     address owner;
 
-    //the minimum ammount of voters to pass a transaction
-    uint public minVoter;
+    //the minimum ammount of OK votes to pass a transaction
+    uint public minVotes;
     uint public nextId = 1;
 
     struct Transaction {
@@ -22,9 +24,9 @@ contract DemocraticWallet{
     }
     mapping(uint => Transaction) transactions;
 
-    //mapping of who voted on wich proposition. votes[voterAdress][trasnctionId] => vote
+    //mapping of who voted on wich proposition. votes[transactionId][voter] => vote
     //Cannot be integrated as a voters in Transaction structure, would imply dynamically creating mappings 
-    mapping(address => mapping(uint => bool)) votes;
+    mapping(uint => mapping(address => bool)) votes;
 
     event TransactionSent(address recipient, uint ammount);
 
@@ -42,12 +44,16 @@ contract DemocraticWallet{
     // checks wether the transaction must be sent and if it is, send it. If txId=0 all tx must be considered
     modifier mightTriggerSendingTransaction(uint txId){
         _;
-        if (transactions[txId].sent){ return; }
-        if(txId == 0){
 
-        }else{
-
+        if(txId == 0){ //if all transaction must be considered
+            for(uint i = 0 ; i < nextId ; i++){
+                _sendTxIfNeeded(i);
+            }
         }
+        else{
+            _sendTxIfNeeded(txId);
+        }
+            
     }
 
     constructor() payable{
@@ -59,16 +65,42 @@ contract DemocraticWallet{
         nextId++;
     }
     function approveTransaction(uint txId) onlyVoter() mightTriggerSendingTransaction(txId) external{
-        votes[msg.sender][txId] = true;
+        votes[txId][msg.sender] = true;
     }
     function disaproveTransaction(uint txId) onlyVoter() public{ //if a transaction was previously approved, dissaprve it
-        votes[msg.sender][txId] = false;
+        votes[txId][msg.sender] = false;
     }
 
     function addVoter(address newVoter) onlyOwner() external{
+        if(voters[newVoter]){return;} //if its already there do nothing
         voters[newVoter] = true;
+        votersList.push(newVoter);
     }
     function removeVoter(address oldVoter) onlyOwner() mightTriggerSendingTransaction(0) external{
+        if(!voters[oldVoter]){return;} //if its not there do nothing
         voters[oldVoter] = false;
+        uint index;
+        for (uint i = 0 ; i < votersList.length ; i++){
+            if (votersList[i] == oldVoter){
+                index = i;
+                break;
+            }
+        }
+        delete votersList[index];
+    }
+    function _sendTxIfNeeded(uint txId) private{
+        if (transactions[txId].sent){ return; }
+        uint nbVoteTrans = 0;
+        for (uint i = 0 ; i < votersList.length ; i++){
+            if (votes[txId][votersList[i]]){
+                nbVoteTrans++;
+            }
+        }
+        if (nbVoteTrans >= minVotes){
+            transactions[txId].sent = true;
+            address payable to = transactions[txId].recipient;
+            to.transfer(transactions[txId].amount);
+            emit TransactionSent(to, transactions[txId].amount);
+        }
     }
 }
